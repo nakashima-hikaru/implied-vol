45f6dd98d13b036a3dde1622bf1998cac9f100a8
hnakashima <nakashima.alg57@gmail.com>
2025-07-28 06:30:27 +0900

    refactor: update function names and add feature flag for original Taylor expansion

--- src/constants.rs
+++ src/constants.rs
@@ -1,6 +1,5 @@
 pub(crate) const SQRT_PI_OVER_TWO: f64 = f64::from_bits(0x3ff40d931ff62705);
 pub(crate) const SQRT_TWO_PI: f64 = f64::from_bits(0x40040d931ff62705);
-pub(crate) const FRAC_SQRT_TWO_PI: f64 = f64::from_bits(0x3fd9884533d43651);
 pub(crate) const SQRT_THREE: f64 = f64::from_bits(0x3ffbb67ae8584caa);
 pub(crate) const SQRT_TWO_OVER_PI: f64 = f64::from_bits(0x3fe9884533d43651);
 pub(crate) const ONE_OVER_SQRT_THREE: f64 = f64::from_bits(0x3fe279a74590331d);
@@ -8,7 +7,6 @@ pub(crate) const TWO_PI_OVER_SQRT_TWENTY_SEVEN: f64 = f64::from_bits(0x3ff358e1a
 pub(crate) const SQRT_THREE_OVER_THIRD_ROOT_TWO_PI: f64 = f64::from_bits(0x3fee095e112353f7);
 pub(crate) const FOURTH_ROOT_DBL_EPSILON: f64 = f64::from_bits(0x3f20000000000000);
 pub(crate) const SIXTEENTH_ROOT_DBL_EPSILON: f64 = f64::from_bits(0x3fbae89f995ad3ad);
-pub(crate) const SQRT_MIN_POSITIVE: f64 = f64::from_bits(0x2000000000000000);
 
 pub(crate) const SQRT_DBL_MAX: f64 = f64::from_bits(0x5fefffffffffffff);
 
--- src/lets_be_rational.rs
+++ src/lets_be_rational.rs
@@ -1,6 +1,6 @@
 use std::f64::consts::{FRAC_1_SQRT_2, SQRT_2};
 use std::ops::Neg;
-use crate::constants::{DENORMALISATION_CUTOFF, FOURTH_ROOT_DBL_EPSILON, FRAC_SQRT_TWO_PI, HALF_OF_LN_TWO_PI, ONE_OVER_SQRT_THREE, SIXTEENTH_ROOT_DBL_EPSILON, SQRT_DBL_MAX, SQRT_MIN_POSITIVE, SQRT_PI_OVER_TWO, SQRT_THREE, SQRT_THREE_OVER_THIRD_ROOT_TWO_PI, SQRT_TWO_OVER_PI, SQRT_TWO_PI, TWO_PI_OVER_SQRT_TWENTY_SEVEN, VOLATILITY_VALUE_TO_SIGNAL_PRICE_IS_ABOVE_MAXIMUM, VOLATILITY_VALUE_TO_SIGNAL_PRICE_IS_BELOW_INTRINSIC};
+use crate::constants::{DENORMALISATION_CUTOFF, FOURTH_ROOT_DBL_EPSILON, HALF_OF_LN_TWO_PI, ONE_OVER_SQRT_THREE, SIXTEENTH_ROOT_DBL_EPSILON, SQRT_DBL_MAX, SQRT_PI_OVER_TWO, SQRT_THREE, SQRT_THREE_OVER_THIRD_ROOT_TWO_PI, SQRT_TWO_OVER_PI, SQRT_TWO_PI, TWO_PI_OVER_SQRT_TWENTY_SEVEN, VOLATILITY_VALUE_TO_SIGNAL_PRICE_IS_ABOVE_MAXIMUM, VOLATILITY_VALUE_TO_SIGNAL_PRICE_IS_BELOW_INTRINSIC};
 use crate::erf_cody::{erf_cody, erfc_cody, erfcx_cody};
 use crate::MulAdd;
 use crate::normal_distribution::{inverse_norm_cdf, norm_cdf, norm_pdf};
@@ -33,22 +33,39 @@ const SMALL_T_EXPANSION_OF_NORMALISED_BLACK_THRESHOLD: f64 = 2.0 * SIXTEENTH_ROO
 
 #[inline(always)]
 fn asymptotic_expansion_of_scaled_normalised_black(h: f64, t: f64) -> f64 {
+    #[inline(always)]
     const fn a0(_e: f64) -> f64 {2.0}
+    #[inline(always)]
     const fn a1(e: f64) -> f64 {-6.0-2.0*e}
+    #[inline(always)]
     const fn a2(e: f64) -> f64 {30.0+e*(60.0+6.0*e)}
+    #[inline(always)]
     const fn a3(e: f64) -> f64 { -2.1E2+e*(-1.05E3+e*(-6.3E2-30.0*e))}
+    #[inline(always)]
     const fn a4(e: f64) -> f64 { 1.89E3+e*(1.764E4+e*(2.646E4+e*(7.56E3+2.1E2*e)))}
+    #[inline(always)]
     const fn a5(e: f64) -> f64 {-2.079E4+e*(-3.1185E5+e*(-8.7318E5+e*(-6.237E5+e*(-1.0395E5-1.89E3*e))))}
+    #[inline(always)]
     const fn a6(e: f64) -> f64 {2.7027E5+e*(5.94594E6+e*(2.675673E7+e*(3.567564E7+e*(1.486485E7+e*(1.62162E6+2.079E4*e)))))}
+    #[inline(always)]
     const fn a7(e: f64) -> f64 {-4.05405E6+e*(-1.2297285E8+e*(-8.1162081E8+e*(-1.73918745E9+e*(-1.35270135E9+e*(-3.6891855E8+e*(-2.837835E7-2.7027E5*e))))))}
+    #[inline(always)]
     const fn a8(e: f64) -> f64 {6.891885E7+e*(2.756754E9+e*(2.50864614E10+e*(7.88431644E10+e*(9.85539555E10+e*(5.01729228E10+e*(9.648639E9+e*(5.513508E8+4.05405E6*e)))))))}
+    #[inline(always)]
     const fn a9(e: f64) -> f64 {-1.30945815E9+e*(-6.678236565E10+e*(-8.013883878E11+e*(-3.4726830138E12+e*(-6.3665855253E12+e*(-5.2090245207E12+e*(-1.8699062382E12+e*(-2.671294626E11+e*(-1.178512335E10-6.891885E7*e))))))))}
+    #[inline(always)]
     const fn a10(e: f64) -> f64 {2.749862115E10+e*(1.7415793395E12+e*(2.664616389435E13+e*(1.52263793682E14+e*(3.848890340295E14+e*(4.618668408354E14+e*(2.664616389435E14+e*(7.10564370516E13+e*(7.83710702775E12+e*(2.749862115E11+1.30945815E9*e)))))))))}
+    #[inline(always)]
     const fn a11(e: f64) -> f64 {-6.3246828645E11+e*(-4.870005805665E13+e*(-9.2530110307635E14+e*(-6.74147946527055E15+e*(-2.24715982175685E16+e*(-3.71802806872497E16+e*(-3.14602375045959E16+e*(-1.34829589305411E16+e*(-2.77590330922905E15+e*(-2.4350029028325E14+e*(-6.95715115095E12-2.749862115E10*e))))))))))}
+    #[inline(always)]
     const fn a12(e: f64) -> f64 {1.581170716125E13+e*(1.454677058835E15+e*(3.36030400590885E16+e*(3.04027505296515E17+e*(1.292_116_897_510_188_8E18+e*(2.81916414002223E18+e*(3.289024830025935E18+e*(2.067387036016302E18+e*(6.840_618_869_171_588E17+e*(1.12010133530295E17+e*(8.0007238235925E15+e*(1.89740485935E14+6.3246828645E11*e)))))))))))}
+    #[inline(always)]
     const fn a13(e: f64) -> f64 {-4.2691609335375E14+e*(-4.624924344665625E16+e*(-1.2764791191277125E18+e*(-1.404_127_031_040_483_7E19+e*(-7.410_670_441_602_553E19+e*(-2.061_513_777_391_255_6E20+e*(-3.171_559_657_525_009E20+e*(-2.748_685_036_521_674_2E20+e*(-1.333_920_679_488_459_6E20+e*(-3.510_317_577_601_209_5E19+e*(-4.680_423_436_801_613E18+e*(-2.774954606799375E17+e*(-5.54990921359875E15-1.581170716125E13*e))))))))))))}
+    #[inline(always)]
     const fn a14(e: f64) -> f64 {1.238056670725875E16+e*(1.5599514051146025E18+e*(5.069_842_066_622_458E19+e*(6.663_221_001_846_66E20+e*(4.275_566_809_518_273E21+e*(1.477_013_988_742_676E22+e*(2.897_219_747_149_095_4E22+e*(3.311_108_282_456_109E22+e*(2.215_520_983_114_014E22+e*(8.551_133_619_036_546E21+e*(1.832_385_775_507_831_3E21+e*(2.027_936_826_648_983_3E20+e*(1.013_968_413_324_491_6E19+e*(1.733279339016225E17+4.2691609335375E14*e)))))))))))))}
+    #[inline(always)]
     const fn a15(e: f64) -> f64 {-3.8379756792502125E17+e*(-5.565_064_734_912_808E19+e*(-2.103_594_469_797_041_5E21+e*(-3.255_562_869_923_992_8E22+e*(-2.495_931_533_608_394_5E23+e*(-1.048_291_244_115_525_7E24+e*(-2.553_529_953_614_742E24+e*(-3.720_857_932_410_053E24+e*(-3.283_109_940_361_811E24+e*(-1.747_152_073_525_876E24+e*(-5.491_049_373_938_467_5E23+e*(-9.766_688_609_771_979E22+e*(-9.115_576_035_787_18E21+e*(-3.895_545_314_438_965_5E20+e*(-5.756_963_518_875_318E18-1.238056670725875E16*e))))))))))))))}
+    #[inline(always)]
     const fn a16(e: f64) -> f64 {1.266_531_974_152_57E19+e*(2.093_999_530_598_916E21+e*(9.108_897_958_105_285E22+e*(1.639_601_632_458_951_2E24+e*(1.480_195_918_192_108_6E25+e*(7.427_892_244_018_582E25+e*(2.199_798_856_882_426E26+e*(3.980_588_407_692_009_4E26+e*(4.478_161_958_653_511E26+e*(3.142_569_795_546_323E26+e*(1.361_780_244_736_74E26+e*(3.552_470_203_661_060_7E25+e*(5.328_705_305_491_592E24+e*(4.250_819_047_115_799E23+e*(1.570_499_647_949_187E22+e*(2.026_451_158_644_112E20+3.8379756792502125E17*e)))))))))))))))}
     const THRESHOLDS: [f64; 12] = [12.347, 12.958, 13.729, 14.718, 16.016, 17.769, 20.221, 23.816, 29.419, 38.93, 57.171, 99.347];
 
@@ -103,12 +120,15 @@ fn asymptotic_expansion_of_scaled_normalised_black(h: f64, t: f64) -> f64 {
     (t / r) * omega
 }
 
-#[inline]
+#[cfg(not(feature = "use_original_taylor_expansion"))]
+#[inline(always)]
 fn yprime_tail_expansion_rational_function_part(w: f64) -> f64 {
     w * (-2.9999999999994663866 + w * (-1.7556263323542206288E2 + w * (-3.4735035445495633334E3 + w * (-2.7805745693864308643E4 + w * (-8.3836021460741980839E4 - 6.6818249032616849037E4 * w))))) / (1.0 + w * (6.3520877744831739102E1 + w * (1.4404389037604337538E3 + w * (1.4562545638507033944E4 + w * (6.6886794165651675684E4 + w * (1.2569970380923908488E5 + 6.9286518679803751694E4 * w))))))
 }
 
-fn yprime(h: f64) -> f64 {
+#[cfg(not(feature = "use_original_taylor_expansion"))]
+#[inline(always)]
+fn y_prime(h: f64) -> f64 {
     // We copied the thresholds of -0.46875 and -4 from Cody.
     if h < -4.0 {
         // Nonlinear-Remez optimized minimax rational function of order (5,6) for g(w) := (Y'(h)/h²-1)/h² with w:=1/h².
@@ -124,24 +144,33 @@ fn yprime(h: f64) -> f64 {
     }
 }
 
+#[cfg(not(feature = "use_original_taylor_expansion"))]
 fn small_t_expansion_of_scaled_normalised_black(h: f64, t: f64) -> f64 {
-    let a = yprime(h);
+    let a = y_prime(h);
     let h2 = h * h;
     let t2 = t * t;
+    #[inline(always)]
     fn b0(a: f64) -> f64 { 2.0 * a}
+    #[inline(always)]
     fn b1(a: f64, h2: f64) -> f64 {(-1.0+a*(3.0+h2))/3.0}
+    #[inline(always)]
     fn b2(a: f64, h2: f64) -> f64 {(-7.0+h2*(10.0+h2)+a*(15.0+h2*(10.0+h2)))/60.0}
+    #[inline(always)]
     fn b3(a: f64, h2: f64) -> f64 {(-57.0+(-18.0-h2)*h2+a*(105.0+h2*(105.0+h2*(21.0+h2))))/2520.0}
+    #[inline(always)]
     fn b4(a: f64, h2: f64) -> f64 {(-561.0+h2*(-285.0+(-33.0-h2)*h2)+a*(945.0+h2*(1260.0+h2*(378.0+h2*(36.0+h2)))))/181440.0}
+    #[inline(always)]
     fn b5(a: f64, h2: f64) -> f64 {(-6555.0+h2*(-4680.0+h2*(-840.0+(-52.0-h2)*h2))+a*(10395.0+h2*(17325.0+h2*(6930.0+h2*(990.0+h2*(55.0+h2))))))/19958400.0}
+    #[inline(always)]
     fn b6(a: f64, h2: f64) -> f64 {(-89055.0+h2*(-82845.0+h2*(-20370.0+h2*(-1926.0+(-75.0-h2)*h2)))+a*(135135.0+h2*(270270.0+h2*(135135.0+h2*(25740.0+h2*(2145.0+h2*(78.0+h2)))))))/3113510400.0}
     t * (b0(a) + t2 * (b1(a, h2) + t2 * (b2(a, h2) + t2 * (b3(a, h2) + t2 * (b4(a, h2) + t2 * (b5(a, h2) + b6(a, h2) * t2))))))
 }
 
+#[cfg(feature = "use_original_taylor_expansion")]
 #[inline(always)]
-fn small_t_expansion_of_normalised_black_call_over_vega(h: f64, t: f64) -> f64 {
-    let w = t.powi(2);
-    let h2 = h.powi(2);
+fn small_t_expansion_of_scaled_normalised_black(h: f64, t: f64) -> f64 {
+    let w = t * t;
+    let h2 = h * h;
     let a = 1_f64 + h * SQRT_PI_OVER_TWO * erfcx_cody(-FRAC_1_SQRT_2 * h);
     let b_over_vega = 2.0 * t * (a + w * ((-1.0 + 3.0 * a + a * h2) / 6.0 + w * ((-7.0 + 15.0 * a + h2 * (-1.0 + 10.0 * a + a * h2)) / 120.0 + w * ((-57.0 + 105.0 * a + h2 * (-18.0 + 105.0 * a + h2 * (-1.0 + 21.0 * a + a * h2))) / 5040.0 + w * ((-561.0 + 945.0 * a + h2 * (-285.0 + 1260.0 * a + h2 * (-33.0 + 378.0 * a + h2 * (-1.0 + 36.0 * a + a * h2)))) / 362880.0 + w * ((-6555.0 + 10395.0 * a + h2 * (-4680.0 + 17325.0 * a + h2 * (-840.0 + 6930.0 * a + h2 * (-52.0 + 990.0 * a + h2 * (-1.0 + 55.0 * a + a * h2))))) / 39916800.0 + ((-89055.0 + 135135.0 * a + h2 * (-82845.0 + 270270.0 * a + h2 * (-20370.0 + 135135.0 * a + h2 * (-1926.0 + 25740.0 * a + h2 * (-75.0 + 2145.0 * a + h2 * (-1.0 + 78.0 * a + a * h2)))))) * w) / 6227020800.0))))));
     b_over_vega.max(0.0).abs()
@@ -205,7 +234,7 @@ fn normalised_black(x: f64, s: f64) -> f64 {
         return asymptotic_expansion_of_scaled_normalised_black(x / s, 0.5 * s) * normalised_vega(x, s);
     }
     if 0.5 * s < SMALL_T_EXPANSION_OF_NORMALISED_BLACK_THRESHOLD {
-        return small_t_expansion_of_normalised_black_call_over_vega(x / s, 0.5 * s) * normalised_vega(x, s);
+        return small_t_expansion_of_scaled_normalised_black(x / s, 0.5 * s) * normalised_vega(x, s);
     }
     normalised_black_call_with_optimal_use_of_codys_functions(x, s)
 }
@@ -225,7 +254,7 @@ fn normalised_black_call_over_vega_and_ln_vega(x: f64, s: f64) -> (f64, f64) {
         return (asymptotic_expansion_of_scaled_normalised_black(x / s, 0.5 * s), ln_vega);
     }
     if 0.5 * s < SMALL_T_EXPANSION_OF_NORMALISED_BLACK_THRESHOLD {
-        return (small_t_expansion_of_normalised_black_call_over_vega(x / s, 0.5 * s), ln_vega);
+        return (small_t_expansion_of_scaled_normalised_black(x / s, 0.5 * s), ln_vega);
     }
     (normalised_black_call_with_optimal_use_of_codys_functions(x, s) * (-ln_vega).exp(), ln_vega)
 }
