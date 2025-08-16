fd56e5999a953c1df7e59a3759192e49477d3199
hnakashima <nakashima.alg57@gmail.com>
2025-08-16 22:52:37 +0900

    refactor: add `#[must_use]` attribute to FFI functions for improved safety and usage clarity

--- src/cxx.rs
+++ src/cxx.rs
@@ -4,12 +4,12 @@ pub mod ffi {
     unsafe extern "C++" {
         include!("lets_be_rational.h");
 
-        pub fn ImpliedBlackVolatility(price: f64, F: f64, K: f64, T: f64, q: f64) -> f64;
-        pub fn Black(F: f64, K: f64, sigma: f64, T: f64, q: f64) -> f64;
+        #[must_use] pub fn ImpliedBlackVolatility(price: f64, F: f64, K: f64, T: f64, q: f64) -> f64;
+        #[must_use] pub fn Black(F: f64, K: f64, sigma: f64, T: f64, q: f64) -> f64;
 
         include!("ImpliedNormalVolatility.cpp");
 
-        pub fn ImpliedNormalVolatility(price: f64, F: f64, K: f64, T: f64, q: f64) -> f64;
-        pub fn Bachelier(F: f64, K: f64, sigma: f64, T: f64, q: f64) -> f64;
+        #[must_use] pub fn ImpliedNormalVolatility(price: f64, F: f64, K: f64, T: f64, q: f64) -> f64;
+        #[must_use] pub fn Bachelier(F: f64, K: f64, sigma: f64, T: f64, q: f64) -> f64;
     }
 }
