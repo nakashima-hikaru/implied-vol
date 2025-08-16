44126ab45ec08f5f327b0971443ddd1637fa477c
hnakashima <nakashima.alg57@gmail.com>
2025-08-16 23:55:28 +0900

    style: apply formatter

--- src/cxx.rs
+++ src/cxx.rs
@@ -4,12 +4,16 @@ pub mod ffi {
     unsafe extern "C++" {
         include!("lets_be_rational.h");
 
-        #[must_use] pub fn ImpliedBlackVolatility(price: f64, F: f64, K: f64, T: f64, q: f64) -> f64;
-        #[must_use] pub fn Black(F: f64, K: f64, sigma: f64, T: f64, q: f64) -> f64;
+        #[must_use]
+        pub fn ImpliedBlackVolatility(price: f64, F: f64, K: f64, T: f64, q: f64) -> f64;
+        #[must_use]
+        pub fn Black(F: f64, K: f64, sigma: f64, T: f64, q: f64) -> f64;
 
         include!("ImpliedNormalVolatility.cpp");
 
-        #[must_use] pub fn ImpliedNormalVolatility(price: f64, F: f64, K: f64, T: f64, q: f64) -> f64;
-        #[must_use] pub fn Bachelier(F: f64, K: f64, sigma: f64, T: f64, q: f64) -> f64;
+        #[must_use]
+        pub fn ImpliedNormalVolatility(price: f64, F: f64, K: f64, T: f64, q: f64) -> f64;
+        #[must_use]
+        pub fn Bachelier(F: f64, K: f64, sigma: f64, T: f64, q: f64) -> f64;
     }
 }
