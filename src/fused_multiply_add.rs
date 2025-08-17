e2ddd3a6e350273eb65ba59d05258bdf5c64c9fa
hnakashima <nakashima.alg57@gmail.com>
2025-08-17 16:44:01 +0900

    refactor: simplify `MulAdd` implementation by merging FMA handling into trait methods

--- src/fused_multiply_add.rs
+++ src/fused_multiply_add.rs
@@ -2,21 +2,16 @@ pub trait MulAdd {
     fn mul_add2(self, a: f64, b: f64) -> f64;
 }
 
-#[cfg(feature = "fma")]
-#[inline(always)]
-fn fma_function(r: f64, a: f64, b: f64) -> f64 {
-    r.mul_add(a, b)
-}
-
-#[cfg(not(feature = "fma"))]
-#[inline(always)]
-fn fma_function(r: f64, a: f64, b: f64) -> f64 {
-    a * r + b
-}
-
 impl MulAdd for f64 {
+    #[cfg(not(feature = "fma"))]
     #[inline(always)]
     fn mul_add2(self, a: f64, b: f64) -> f64 {
-        fma_function(self, a, b)
+        a * self + b
+    }
+
+    #[cfg(feature = "fma")]
+    #[inline(always)]
+    fn mul_add2(self, a: f64, b: f64) -> f64 {
+        self.mul_add(a, b)
     }
 }
